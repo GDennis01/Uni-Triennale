657aec3959ef7bc70ed349c50fe567dfe4809a4a
Dennis <dennis.gobbi001@gmail.com>
2021-11-07 17:20:48 +0100

    Finito es file shuffle

--- Sistemi Operativi/EserciziFileC/file_shuffle_rows.c
+++ Sistemi Operativi/EserciziFileC/file_shuffle_rows.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/types.h>//
+#include <unistd.h>//Libs needed for getpid() method
 
   
 //scrive nel file argv2 le righe di argv1 in ordine casuale
@@ -19,7 +19,8 @@ int main(int argc, char const *argv[]){
 
 int file_shuffle_rows(FILE*file1,FILE*file2){
     srand(getpid());
-    //Bisogna prima capire come leggere un file :(
+
+    //Lettura righe file
     char **buffer=malloc(1024);//malloc provvisoria di 1024 bytes
     char charac=0;
     int i=0,j=0;
@@ -27,15 +28,24 @@ int file_shuffle_rows(FILE*file1,FILE*file2){
         buffer[i]=malloc(1);
         while((char)(charac =fgetc(file1)) != '\n' && charac != EOF){//se incontro un \n o un EOF, esco dal ciclo
             buffer[i][j]=charac;
-            buffer[i]=realloc(buffer[i],sizeof(buffer[i])+1+j);
+            buffer[i]=realloc(buffer[i],sizeof(buffer[i])+1+j);//a ogni lettura di un singolo carattere, realloco la memoria di mem+1
             j++;
-           
         }
           printf("Riga:%s",buffer[i]);
         j=0;
         printf("\n");
     i++;
     }
-    
+    //Fine lettura righe
+
+    //Ora incollo in ordine casuale le righe del file1 nel file2
+    int z=0;
+    while(z<i){//todo
+    int a=rand()%i;
+    int pos=fseek(file2,0,SEEK_END);//SEEK_END per l'append
+    fprintf(file2,"%s\n",buffer[a]);
+    z++;
+
+    }
     return 1;//error with writing files
 }
